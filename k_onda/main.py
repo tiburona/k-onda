aa3ff0d8a718b56e231d6cc35c612c1395d3b257
Katie Surrence <katie@Katies-MBP.fibertel.com.ar>
2025-05-19 13:25:04 -0300

    fix tests

--- k_onda/main.py
+++ k_onda/main.py
@@ -1,20 +1,12 @@
 
-from k_onda.utils import log_directory_contents, load_config_py
+from k_onda.utils import log_directory_contents
 from k_onda.run import Runner
 
 
 DEFAULT_CONFIG = '/path/to/init_config.json'
 
-SAFETY_CONFIG = '/Users/katie/likhtik/k-onda-analysis/IG_INED_Safety/config/init_config.json'
-POWER_OPTS = '/Users/katie/likhtik/k-onda-analysis/IG_INED_Safety/config/ig_ined_safety_power_opts.py'
-PY_OPTS = load_config_py(POWER_OPTS)
-PREP = PY_OPTS.PREP_OPTS
-#OPTS = PY_OPTS.POWER_PLOT_OPTS
-OPTS = PY_OPTS.GROUP_CHECK_OPTS
 
-
-
-def run_pipeline(config_file=SAFETY_CONFIG, opts=OPTS, prep=PREP, logdir=None):
+def run_pipeline(config_file=DEFAULT_CONFIG, opts=None, prep=None, logdir=None):
     """Run the core analysis pipeline using user-supplied options."""
     runner = Runner(config_file=config_file)
 
